5431be40f62eb638a7c55d37a948d927689a2c76
gbdev83 <bleingauthier@gmail.com>
2023-07-03 21:16:39 +0200

    changes on login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -10,7 +10,7 @@ export default function Auth2() {
 
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch('https://airdrophunter-api.adaptable.app/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
